7eeda2dd59f394fc3ff14746b32241b18695368f
Alejo Conde <alejo.conde.97@gmail.com>
2023-10-30 23:32:55 -0300

    part3: exercise 3.15

--- index.js
+++ index.js
@@ -97,11 +97,12 @@ app.post('/api/persons', (request, response) => {
   })
 })
 
-app.delete('/api/persons/:id',(request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(persons => persons.id !== id)
-
-  response.status(204).end()
+app.delete('/api/persons/:id',(request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+  .then(result => {
+    response.status(204).end()
+  })
+  .catch(error => next(error))
 })
 
 const PORT = process.env.PORT
